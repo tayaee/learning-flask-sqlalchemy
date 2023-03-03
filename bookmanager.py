6a61f02b3336922047d6fde3eebbcdbbafe6dc10
Tayaee <soonki.ji@gmail.com>
2023-03-03 01:14:31 -0500

    Added Add Book, Books, flash()

--- bookmanager.py
+++ bookmanager.py
@@ -1,22 +1,46 @@
+import os.path
+
 from flask import Flask
+from flask import flash
 from flask import render_template
 from flask import request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+# db
+project_dir = os.path.dirname(os.path.abspath(__file__))
+db_file = f'sqlite:///{os.path.join(project_dir, "book.db")}'
+app.config['SECRET_KEY'] = 'A secret key'
+app.config['SQLALCHEMY_DATABASE_URI'] = db_file
+db = SQLAlchemy(app)
 
 
-def create_app():
-    app = Flask(__name__)
+class Book(db.Model):
+    title = db.Column(db.String(80), unique=True, nullable=False, primary_key=True)
 
-    @app.route('/', methods=["GET", "POST"])
-    def home():
-        if request.form:
-            print(request.form)
-            data = request.get_data()
-            print(data)
-        return render_template('home.html')
+    def __repr__(self):
+        return f"<Title: {self.title}"
 
-    return app
+
+@app.route('/', methods=["GET", "POST"])
+def home():
+    if request.form:
+        title = request.form.get('title')
+        error = None
+        if title:
+            book = Book(title=title)
+            db.session.add(book)
+            db.session.commit()
+        else:
+            error = 'Title is required.'
+        if error:
+            flash(error)
+    books = Book.query.all()
+    return render_template('home.html', books=books)
 
 
 if __name__ == '__main__':
-    app = create_app()
+    with app.app_context():
+        db.create_all()
     app.run(host='0.0.0.0', port=5000, debug=True)
